6b1dbd1b8a9be2558aef017362a50bf1539f3441
PabloFerreiro <grafpablo@gmail.com>
2023-09-08 17:12:33 -0300

    cambios

--- src/universidadgrupo33/UniversidadGrupo33.java
+++ src/universidadgrupo33/UniversidadGrupo33.java
@@ -1,21 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package universidadgrupo33;
 
-/**
- *
- * @author Pablo
- */
+import universidadgrupo33.accesoADatos.Conexion;
+
 public class UniversidadGrupo33 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Conexion.getConexion();
     }
     
 }
---
+++ src/universidadgrupo33/accesoADatos/Conexion.java
@@ -0,0 +1,50 @@
+
+package universidadgrupo33.accesoADatos;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+public class Conexion {
+    //atributos del tipo estatico y final (constantes)
+    private static final String URL="jdbc:mariadb://localhost/";
+    private static final String DB="universidadgrupo33";
+    private static final String USUARIO="root";
+    private static final String PASSWORD="";
+    private static Connection connection;
+    
+    //constructor vacio
+    private Conexion(){}
+    
+    //Método para conectarse
+    public static Connection getConexion(){
+        
+        //condicional para ver si la conexion es nula
+        if(connection==null){
+            //try catch para las excepciones (del driver y de la conexion)
+            try {
+                
+                //carga y registro del driver
+                Class.forName("org.mariadb.jdbc.Driver");
+                
+                // conexion a la base de datos
+                //connection = DriverManager.getConnection(URL+DB, USUARIO, PASSWORD);
+                
+                connection = DriverManager.getConnection(URL+DB + "?useLegacyDatetimeCode=false&serverTimezone=UTC"
+                  + "&user=" + USUARIO+ "&password=" + PASSWORD);
+                
+                //borrar despues si se quiere este JOptionPane
+                JOptionPane.showMessageDialog(null,"Se conecto a la base de datos");
+                
+            } catch (ClassNotFoundException ex) {
+                JOptionPane.showMessageDialog(null,"Error al cargar los drivers");
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null,"Error al conectarse a la base de datos");
+            }
+        
+        }
+    return connection;
+    }
+    
+}
