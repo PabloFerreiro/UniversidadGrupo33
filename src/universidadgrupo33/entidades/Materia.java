a235e749db68ffba62ef1d00d2773be4a8ee61ec
PabloFerreiro <141683969+PabloFerreiro@users.noreply.github.com>
2023-09-08 15:54:43 -0300

    Update Materia.java

--- src/universidadgrupo33/entidades/Materia.java
+++ src/universidadgrupo33/entidades/Materia.java
@@ -2,7 +2,7 @@ package universidadgrupo33.entidades;
 
 public class Materia 
 {
-    //atributos----------HOLA, muchachos  
+    //atributos----------HOLA, muchachos, como estan? 
     private int idMateria;
     private String nombre;
     private int año;
